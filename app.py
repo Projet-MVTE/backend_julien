4f378a5ddeaac620c3c6a60ebac387d221d247ea
julien-chmitelin <julien.chmitelin@gmail.com>
2024-11-26 11:35:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def set_three(input_string):
         f = open("arbre_decisions.txt","r")
         chaine = f.read()
         f.close()
-        return jsonify({"result": chaine}), 200
+        return jsonify({"Fichier modifié": chaine}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
