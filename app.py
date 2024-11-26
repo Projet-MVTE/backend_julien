6a9d876d629b99bc80995d47ae889d0e3e49e3af
julien-chmitelin <julien.chmitelin@gmail.com>
2024-11-26 08:57:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def example_script(param1=None, param2=None):
     return {"param1": param1, "param2": param2}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
