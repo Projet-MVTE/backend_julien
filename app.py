ed9f1a7b34398f0279fe435c8dfb663a66fe8b73
julien-chmitelin <julien.chmitelin@gmail.com>
2025-01-21 10:02:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,25 +8,13 @@ def home():
     return jsonify({"message": "test 1"}), 200
     
 # Route pour télécharger un fichier JSON
-@app.route("/download_three", methods=["GET"])
+@app.route("/download_tree", methods=["GET"])
 def download_json():
     json_file_path = "arbre.json"
     try:
         return send_file(json_file_path, as_attachment=True, mimetype='application/json')
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-"""
-@app.route("/set_three/<string:input_string>", methods=["GET"])
-def set_three(input_string):
-    try:
-        f = open("arbre_decisions.txt","w")
-        f.write(input_string)
-        f.close()
-        return jsonify({"Fichier modifié": chaine}), 200
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
-"""
 
 @app.route('/set_tree', methods=['POST'])
 def upload_file():
