402cc1e3c86304b769362e017fb0072743939fb1
julien-chmitelin <julien.chmitelin@gmail.com>
2025-04-01 11:27:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -354,6 +354,7 @@ def askIA():
 @app.route('/explanation_file', methods=["GET"])
 def show_explanation_file():
     #test
+    x = 1
     return jsonify({"message":"test"}), send_file('lime_explanation_with_original_values.html', as_attachment=True, mimetype='text/html')  # Charge index.html
 
 if __name__ == "__main__":
