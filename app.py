aafff208bfcfe2dccfb7999c6d621f566e10e169
julien-chmitelin <julien.chmitelin@gmail.com>
2025-04-01 16:33:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -346,7 +346,7 @@ def askIA():
             return jsonify({"error":"chronicInflammatoryDisease not valid, "+str(chronicInflammatoryDisease)})
 
         chirurgie, platre, grossesse, post_partum, hospitalisation, voyage, contraception, menopause, antecedent_MVTE, familial_MVTE, AVC_isch, AVC_hem, pneumopathie, BPCO,hypothyroidie, hyperthyroidie, maladie_renale, cardiopathie_coronarienne, hepatique, rythmique, cancer =  "chirurgie", "platre", "grossesse", "post_partum", "hospitalisation", "voyage", "contraception", "menopause", "antecedent_MVTE", "familial_MVTE", "AVC_isch", "AVC_hem", "pneumopathie", "BPCO","hypothyroidie", "hyperthyroidie", "maladie_renale", "cardiopathie_coronarienne", "hepatique", "rythmique", "cancer"     
-        riskFactorsList = request.args.get('riskFactorsList', type=str).replace('true', True).replace('false', False)
+        riskFactorsList = request.args.get('riskFactorsList', type=str).replace('true', 'True').replace('false', 'False')
         dic = ast.literal_eval(riskFactorsList)
         
         """
